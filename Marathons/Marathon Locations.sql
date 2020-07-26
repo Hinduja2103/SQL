286b3dc7ae334524220b89b7d5f850f7a5ade60a
Hinduja2103 <68810132+Hinduja2103@users.noreply.github.com>
2020-07-26 21:08:50 +0530

    Add files via upload

---
+++ Marathons/Marathon Locations.sql
@@ -0,0 +1,65 @@
+﻿CREATE TABLE locations (id serial,location_name character varying(20),address1 character varying(50),address2 character varying(50),area character varying(50),
+city character varying(20),state character varying(20),country character varying(20),pincode integer,longitude_deg character varying(10),
+lattitude_deg character varying(10));  
+
+SELECT * FROM locations;
+
+INSERT INTO locations(id,location_name,address1,address2,area,city,state,country,pincode,longitude_deg,lattitude_deg) VALUES (1,'User1 Loc',
+'#92,14th Main','Muneeshwara Block','Hanumanthanagar','Banaglore','Karnataka','India',560026,'12.9716N','77.5946E');
+
+INSERT INTO locations(id,location_name,address1,address2,area,city,state,country,pincode,longitude_deg,lattitude_deg) VALUES (2,'User2 Loc',
+'#30,2nd Cross','Varma Layout','Amruthanagar','Banaglore','Karnataka','India',560024,'12.9716N','77.5946E');
+
+INSERT INTO locations(id,location_name,address1,address2,area,city,state,country,pincode,longitude_deg,lattitude_deg) VALUES (3,'User3 Loc',
+'#67,3rd Street','Muniyappa Layout','Vijaynagar','Banaglore','Karnataka','India',560036,'12.9716N','77.5946E');
+
+INSERT INTO locations(id,location_name,address1,address2,area,city,state,country,pincode,longitude_deg,lattitude_deg) VALUES (4,'User4 Loc',
+'#96,5th Street','Muniyappa Layout','Vijaynagar','Banaglore','Karnataka','India',560046,'12.9716N','77.5946E');
+
+INSERT INTO locations(id,location_name,address1,address2,area,city,state,country,pincode,longitude_deg,lattitude_deg) VALUES (5,'User5 Loc',
+'#106,6th Main','Bhadrappa Layout','Koti Hosahalli','Banaglore','Karnataka','India',560055,'12.9716N','77.5946E');
+
+INSERT INTO locations(id,location_name,address1,address2,area,city,state,country,pincode,longitude_deg,lattitude_deg) VALUES (6,'User6 Loc',
+'#22,10th Cross','Telecom Layout','Dasarahalli','Banaglore','Karnataka','India',560092,'12.9716N','77.5946E');
+
+INSERT INTO locations(id,location_name,address1,address2,area,city,state,country,pincode,longitude_deg,lattitude_deg) VALUES (7,'User7 Loc',
+'#45,12th Cross','MCECHS Layout','Hegdenagar','Banaglore','Karnataka','India',560045,'12.9716N','77.5946E');
+
+INSERT INTO locations(id,location_name,address1,address2,area,city,state,country,pincode,longitude_deg,lattitude_deg) VALUES (8,'User8 Loc',
+'#13,7th Street','Kogilu Cross','Yelahanka','Banaglore','Karnataka','India',560046,'12.9716N','77.5946E');
+
+INSERT INTO locations(id,location_name,address1,address2,area,city,state,country,pincode,longitude_deg,lattitude_deg) VALUES (9,'User9 Loc',
+'#57,4th Block','Bannerghatta Road','Bannerghatta','Banaglore','Karnataka','India',560046,'12.9716N','77.5946E');
+
+INSERT INTO locations(id,location_name,address1,address2,area,city,state,country,pincode,longitude_deg,lattitude_deg) VALUES (10,'User10 Loc',
+'#57,4th Block','Bannerghatta Road','Bannerghatta','Banaglore','Karnataka','India',560046,'12.9716N','77.5946E');
+
+INSERT INTO locations(id,location_name,address1,address2,area,city,state,country,pincode,longitude_deg,lattitude_deg) VALUES (11,'User11 Loc',
+'#68,3rd Street','Road no.4','Banjara Hills','Hyderabad','Telangana','India',500034,'17.3850N','78.4867E');
+
+INSERT INTO locations(id,location_name,address1,address2,area,city,state,country,pincode,longitude_deg,lattitude_deg) VALUES (12,'User12 Loc',
+'Road no.5','Uppal','Gaddi annaram','Hyderabad','Telangana','India',500036,'17.3850N','78.4867E');
+
+INSERT INTO locations(id,location_name,address1,address2,area,city,state,country,pincode,longitude_deg,lattitude_deg) VALUES (13,'User13 Loc',
+'#98/3,Srinivasapura Village','Bella Halli','Yelahanka','Bangalore','Karnataka','India',560064,'12.9716N','77.5946E');
+
+INSERT INTO locations(id,location_name,address1,address2,area,city,state,country,pincode,longitude_deg,lattitude_deg) VALUES (14,'User14 Loc',
+'33,2B, Kempapura Main Road','Mariyannapalya','Hebbal','Bangalore','Karnataka','India',560045,'12.9716N','77.5946E');
+
+INSERT INTO locations(id,location_name,address1,address2,area,city,state,country,pincode,longitude_deg,lattitude_deg) VALUES (15,'User15 Loc',
+'Chokkanahalli','Thanisandra','Yelahanka','Bangalore','Karnataka','India',560024,'12.9716N','77.5946E');
+
+INSERT INTO locations(id,location_name,address1,address2,area,city,state,country,pincode,longitude_deg,lattitude_deg) VALUES (16,'User16 Loc',
+'#271,Stage 1','50 Ft. Main Road','Banashankari','Bangalore','Karnataka','India',560019,'12.9716N','77.5946E');
+
+INSERT INTO locations(id,location_name,address1,address2,area,city,state,country,pincode,longitude_deg,lattitude_deg) VALUES (17,'User17 Loc',
+'#40,4th Block','Kumara Park West','Seshadripuram','Bangalore','Karnataka','India',560020,'12.9716N','77.5946E');
+
+INSERT INTO locations(id,location_name,address1,address2,area,city,state,country,pincode,longitude_deg,lattitude_deg) VALUES (18,'User18 Loc',
+'#2/2,3rd Stage','Ittamadu','Banashankari','Bangalore','Karnataka','India',560085,'12.9716N','77.5946E');
+
+INSERT INTO locations(id,location_name,address1,address2,area,city,state,country,pincode,longitude_deg,lattitude_deg) VALUES (19,'User19 Loc',
+'#1/1A Kanaka Layout','Brindavan Layout','Padmanabhanagar','Bangalore','Karnataka','India',560061,'12.9716N','77.5946E');
+
+INSERT INTO locations(id,location_name,address1,address2,area,city,state,country,pincode,longitude_deg,lattitude_deg) VALUES (20,'User20 Loc',
+'11th Main','Bikaspura Main Road','Kanakapura','Bangalore','Karnataka','India',560062,'12.9716N','77.5946E');
