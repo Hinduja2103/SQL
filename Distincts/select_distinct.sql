1fc284625bbd1463ebadcc09a96bcf364546f592
Hinduja2103 <68810132+Hinduja2103@users.noreply.github.com>
2020-07-26 21:07:09 +0530

    Add files via upload

---
+++ Distincts/select_distinct.sql
@@ -0,0 +1,4 @@
+﻿select * from t1;
+SELECT DISTINCT bcolor FROM t1 ORDER BY bcolor;
+SELECT DISTINCT bcolor,fcolor FROM t1 ORDER BY bcolor,fcolor;
+SELECT DISTINCT ON (bcolor) bcolor,fcolor FROM t1 ORDER BY bcolor,fcolor;
