aa6767c13086320bf34000b01f6127bf9cbaf69d
Hinduja2103 <68810132+Hinduja2103@users.noreply.github.com>
2020-07-26 21:05:18 +0530

    Add files via upload

---
+++ Joins/Self Join.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE self_join (id serial, user_name character varying(20), master_id integer);
+
+INSERT INTO self_join(id,user_name,master_id) VALUES (1,'Jack',Null);
+
+INSERT INTO self_join(id,user_name,master_id) VALUES (2,'Smith',1);
+
+INSERT INTO self_join(id,user_name,master_id) VALUES (3,'John',2);
+
+INSERT INTO self_join(id,user_name,master_id) VALUES (4,'Yash',Null);
+
+INSERT INTO self_join(id,user_name,master_id) VALUES (5,'Radhika',4);
+
+DELETE FROM self_join where user_name = 'Smith';
+
+SELECT * FROM self_join;
+
+select self_join s1, self_join s2
