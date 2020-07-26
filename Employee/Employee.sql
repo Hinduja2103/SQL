ee0b7cd8795fb51c94c43d5d6d4fe4f311448d83
Hinduja2103 <68810132+Hinduja2103@users.noreply.github.com>
2020-07-26 20:54:55 +0530

    Add files via upload

---
+++ Employee/Employee.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE employee(id serial, emp_name character varying(30), emp_password character varying(20), 
+email_id character varying(50),country character varying(20));  
+
+SELECT * FROM employee;
+
+INSERT INTO employee(emp_name, emp_password, email_id, country) values ('Hinduja', 'pinky@123', 'hinduja2gmail.com', 'India');
+
+INSERT INTO employee(emp_name, emp_password, email_id, country) values ('Yahaswini', 'yash@123', 'yash@gmail.com', 'Pakistan');
