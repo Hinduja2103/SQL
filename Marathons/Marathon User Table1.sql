b483e28daae0756e171026c94b94056df802320d
Hinduja2103 <68810132+Hinduja2103@users.noreply.github.com>
2020-07-26 21:09:36 +0530

    Add files via upload

---
+++ Marathons/Marathon User Table1.sql
@@ -0,0 +1,23 @@
+﻿SELECT *FROM marathon_users;
+
+UPDATE marathon_users SET company_school='RED FM' where id=4;
+
+
+select * from marathon_users  where company_school ilike '%inf%'
+
+alter table marathon_users add column pancard character varying(20);
+
+alter table marathon_users add column annual_salary money;
+
+select * from marathon_users  where gender='Female' and company_school ilike '%inf%'
+
+
+aggregate functions
+=====================
+
+count
+sum
+select min(age) from marathon_users group by user_name,age order by  age desc
+
+max
+avg
