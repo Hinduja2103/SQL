fc4e78f10240bfd971650cb8916342d3058a568a
Hinduja2103 <68810132+Hinduja2103@users.noreply.github.com>
2020-07-26 20:54:08 +0530

    Add files via upload

---
+++ Employee/Employee Details Table.sql
@@ -0,0 +1,30 @@
+﻿create table employee_details(Id serial, Emp_Name character varying, Mobile_No character varying, Mail_Id character varying, Emp_Role character varying,
+Salary money);
+
+select * from employee_details;
+
+insert into employee_details(emp_name, mobile_no, mail_id, emp_role, salary) values('Kiran', '1234567890', 'kiran@gmail.com', 'CEO', '200000');
+
+insert into employee_details(emp_name, mobile_no, mail_id, emp_role, salary) values('Anil', '2345678901', 'anil@gmail.com', 'CFO', '200000');
+
+insert into employee_details(emp_name, mobile_no, mail_id, emp_role, salary) values('Hinduja', '5436789012', 'hinduja@gmail.com', 'General Manager', '180000');
+
+insert into employee_details(emp_name, mobile_no, mail_id, emp_role, salary) values('Karthik', '5342678901', 'karthik@gmail.com', 'HR Manager', '150000');
+
+insert into employee_details(emp_name, mobile_no, mail_id, emp_role, salary) values('Anvitha', '4567890321', 'anvitha@gmail.com', 'Technical Manager', '150000');
+
+insert into employee_details(emp_name, mobile_no, mail_id, emp_role, salary) values('Yashaswini', '3456789012', 'yashaswini@gmail.com', 'Technical Manager', '150000');
+
+insert into employee_details(emp_name, mobile_no, mail_id, emp_role, salary) values('Srikanth', '4567890123', 'srikanth@gmail.com', 'Project Engineer', '120000');
+
+insert into employee_details(emp_name, mobile_no, mail_id, emp_role, salary) values('Gaurav', '5678901234', 'gaurav@gmail.com', 'Project Engineer', '120000');
+
+insert into employee_details(emp_name, mobile_no, mail_id, emp_role, salary) values('Shrinivas', '6789012345', 'shirinivas@gmail.com', 'Project Engineer', '120000');
+
+insert into employee_details(emp_name, mobile_no, mail_id, emp_role, salary) values('Manoj', '3241890321', 'manoj@gmail.com', 'Product Manager', '100000');
+
+insert into employee_details(emp_name, mobile_no, mail_id, emp_role, salary) values('Yogitha', '5673490321', 'yogitha@gmail.com', 'Product Manager', '100000');
+
+insert into employee_details(emp_name, mobile_no, mail_id, emp_role, salary) values('Revanth', '7658902314', 'revanth@gmail.com', 'Field Manager', '100000');
+
+
