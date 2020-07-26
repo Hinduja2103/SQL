8e92babbd60e05607ea5591c3db2d4bad96a5ac1
Hinduja2103 <68810132+Hinduja2103@users.noreply.github.com>
2020-07-26 20:51:56 +0530

    Add files via upload

---
+++ functions.sql
@@ -0,0 +1,10 @@
+﻿CREATE FUNCTION dup(in int, out f1 int, out f2 text)
+    AS $$ SELECT $1, CAST($1 AS text) || ' is text' $$
+    LANGUAGE SQL;
+
+CREATE FUNCTION test(in int)
+    AS $$ SELECT $1|| ' is text' $$
+    LANGUAGE SQL;
+
+
+    select dup(10)
