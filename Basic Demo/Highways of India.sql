7c2f9af49dfba0192457ee47c2cd9919942db0cc
Hinduja2103 <68810132+Hinduja2103@users.noreply.github.com>
2020-07-26 20:57:02 +0530

    Add files via upload

---
+++ Basic Demo/Highways of India.sql
@@ -0,0 +1,31 @@
+﻿CREATE TABLE Highways_of_India(id serial, Highway_Name character varying (10), Start_location character varying(50), End_loaction character varying(50), Distance_in_km integer);
+
+SELECT * FROM Highways_of_India;
+
+INSERT INTO Highways_of_India(Highway_Name, Start_location, End_loaction,Distance_in_km) VALUES ('NH7', 'Varanasi', 'Kanyakumari', '2369');
+
+INSERT INTO Highways_of_India(Highway_Name, Start_location, End_loaction,Distance_in_km) VALUES ('NH6', 'Hajira', 'Kolkata', '1949');
+
+INSERT INTO Highways_of_India(Highway_Name, Start_location, End_loaction,Distance_in_km) VALUES ('NH5', 'Cuttack', 'Chennai', '1533');
+
+INSERT INTO Highways_of_India(Highway_Name, Start_location, End_loaction,Distance_in_km) VALUES ('NH15', 'Pathankot', 'Samakhiali', '1526');
+
+INSERT INTO Highways_of_India(Highway_Name, Start_location, End_loaction,Distance_in_km) VALUES ('NH2', 'Delhi', 'Dankuni', '1465');
+
+INSERT INTO Highways_of_India(Highway_Name, Start_location, End_loaction,Distance_in_km) VALUES ('NH8', 'Delhi', 'Mumbai', '1428');
+
+INSERT INTO Highways_of_India(Highway_Name, Start_location, End_loaction,Distance_in_km) VALUES ('NH17', 'Panvel', 'Kochi', '1269');
+
+INSERT INTO Highways_of_India(Highway_Name, Start_location, End_loaction,Distance_in_km) VALUES ('NH4', 'Thane', 'Chennai', '1235');
+
+INSERT INTO Highways_of_India(Highway_Name, Start_location, End_loaction,Distance_in_km) VALUES ('NH3', 'Agra', 'Mumbai', '1161');
+
+INSERT INTO Highways_of_India(Highway_Name, Start_location, End_loaction,Distance_in_km) VALUES ('NH31', 'Barhi', 'Guwahati', '1125');
+
+
+
+
+
+
+
+
