d36f37afb46eb2c66b5fb691db5e76a377ffce8c
Hinduja2103 <68810132+Hinduja2103@users.noreply.github.com>
2020-07-26 21:01:17 +0530

    Add files via upload

---
+++ renamingtableandcolumn.sql
@@ -0,0 +1,13 @@
+﻿
+
+create table yash_table(id serial, uname character varying);
+
+
+select * from hinduja_table
+
+insert into yash_table( id,uname) values(1,'aswini');
+
+alter table yash_table rename to hinduja_table;
+
+alter table hinduja_table add column age integer;
+
